313d8de5b31affc34883a7271a4fb36eca6e1988
Toon251 <toon251@gmail.com>
2025-05-06 02:45:39 +0000

    Test add index method in bookmark controller

--- plugin.rb
+++ plugin.rb
@@ -11,7 +11,24 @@ after_initialize do
   end
 
   Discourse::Application.routes.append do
-    post '/bookmarks' => 'bookmarks#create'
-    delete '/bookmarks/:id' => 'bookmarks#destroy'
+    get '/bookmarks' =-> 'bookmarks#index'
+    # post '/bookmarks' => 'bookmarks#create'
+    # delete '/bookmarks/:id' => 'bookmarks#destroy'
+  end
+
+  class BookmarksController < ApplicationController
+    def index
+      # ดึงข้อมูล bookmark ของ current_user
+      @bookmarks = Bookmark.where(user_id: current_user.id)
+  
+      # รองรับทั้ง HTML และ JSON
+      respond_to do |format|
+        format.html { render :index }
+        format.json { render json: @bookmarks } # JSON handler
+      end
+    rescue StandardError => e
+      # จัดการ error หากมี
+      render json: { success: false, error: e.message }, status: :unprocessable_entity
+    end
   end
 end
